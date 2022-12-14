28bf373465a7033726b630c07db16b0806b4658e
john.torcivia <john.torcivia@clarifai.com>
2022-12-14 10:11:14 -0500

    Updates for icons

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import {addIcon, Notice, Plugin, MarkdownView, Editor,MarkdownRenderer,MarkdownP
 import {ExampleModal} from './model';
 import {ClarifaiTextGeneratorSettings, Context} from './types';
 
-import {GENERATE_ICON,GENERATE_META_ICON} from './constants';
+import {GENERATE_META_ICON,WRITE_ICON,CREATE_IMAGE} from './constants';
 import TextGeneratorSettingTab from './ui/settingsPage';
 import {SetMaxTokens} from './ui/setMaxTokens';
 import TextGenerator from './textGenerator';
@@ -102,9 +102,10 @@ export default class TextGeneratorPlugin extends Plugin {
     }
 	
 	async onload() {
-		// TODO: Use clarifai icon?
-		addIcon("GENERATE_ICON",GENERATE_ICON);
-		addIcon("GENERATE_META_ICON",GENERATE_META_ICON);
+		// addIcon("GENERATE_ICON",GENERATE_ICON);
+		addIcon("WRITE_ICON", WRITE_ICON);
+		addIcon("CREATE_IMAGE", CREATE_IMAGE);
+		// addIcon("GENERATE_META_ICON",GENERATE_META_ICON);
 		await this.loadClarifaiSettings();
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new TextGeneratorSettingTab(this.app, this));
@@ -114,7 +115,7 @@ export default class TextGeneratorPlugin extends Plugin {
 		this.app.workspace.updateOptions();
 		this.statusBarItemEl = this.addStatusBarItem();
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('GENERATE_ICON', 'Generate Text!', async (evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('WRITE_ICON', 'Generate Text!', async (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
 			const activeFile = this.app.workspace.getActiveFile();
 			const activeView = this.getActiveView();
@@ -128,17 +129,45 @@ export default class TextGeneratorPlugin extends Plugin {
 			}
 		});
 
-		// TODO: Example of how to add a ribbon icon; maybe a link to Clarifai?
+		// TODO: This needs to be updated to support generating an image!
+		const ribbonIconEl2 = this.addRibbonIcon('CREATE_IMAGE', 'Generate Image!', async (evt: MouseEvent) => {
+			// Called when the user clicks the icon.
+			const activeFile = this.app.workspace.getActiveFile();
+			const activeView = this.getActiveView();
+			if (activeView !== null) {
+			const editor = activeView.editor;
+			try {
+				await this.textGenerator.generateInEditor(this.clarifaiSettings,false,editor);
+			} catch (error) {
+				this.handelError(error);
+			}
+			}
+		});
+
 		// const ribbonIconEl2 = this.addRibbonIcon('boxes', 'Text Generator: Templates Packages Manager', async (evt: MouseEvent) => {
 		// 	new PackageManagerUI(this.app,this,async (result: string) => {
 		// 	}).open();
 		// });
 
-
 		this.addCommand({
 			id: 'generate-text',
 			name: 'Generate Text!',
-			icon: 'GENERATE_ICON',
+			icon: 'WRITE_ICON',
+			hotkeys: [{ modifiers: ["Mod"], key: "j" }],
+			editorCallback: async (editor: Editor) => {
+				try {
+					await this.textGenerator.generateInEditor(this.clarifaiSettings,false,editor);
+				} catch (error) {
+					this.handelError(error);
+				}	
+			}
+		});
+
+		// TODO: This is the generate image call that needs to be updated
+		this.addCommand({
+			id: 'generate-image',
+			name: 'Generate Image!',
+			icon: 'CREATE_IMAGE',
 			hotkeys: [{ modifiers: ["Mod"], key: "j" }],
 			editorCallback: async (editor: Editor) => {
 				try {
@@ -350,7 +379,8 @@ export default class TextGeneratorPlugin extends Plugin {
 	addTGMenu(el:HTMLElement,markdown:string,source:string) {
 				const div = document.createElement('div');
 				div.classList.add('tgmenu');
-				const generateSVG= `<svg viewBox="0 0 100 100" class="svg-icon GENERATE_ICON"><defs><style>.cls-1{fill:none;stroke:currentColor;stroke-linecap:round;stroke-linejoin:round;stroke-width:4px;}</style></defs><g id="Layer_2" data-name="Layer 2"><g id="VECTOR"><rect class="cls-1" x="74.98" y="21.55" width="18.9" height="37.59"></rect><path class="cls-1" d="M38.44,27.66a8,8,0,0,0-8.26,1.89L24.8,34.86a25.44,25.44,0,0,0-6,9.3L14.14,56.83C11.33,64.7,18.53,67.3,21,60.9" transform="translate(-1.93 -15.75)"></path><polyline class="cls-1" points="74.98 25.58 56.61 18.72 46.72 15.45"></polyline><path class="cls-1" d="M55.45,46.06,42.11,49.43,22.76,50.61c-8.27,1.3-5.51,11.67,4.88,12.8L46.5,65.78,53,68.4a23.65,23.65,0,0,0,17.9,0l6-2.46" transform="translate(-1.93 -15.75)"></path><path class="cls-1" d="M37.07,64.58v5.91A3.49,3.49,0,0,1,33.65,74h0a3.49,3.49,0,0,1-3.45-3.52V64.58" transform="translate(-1.93 -15.75)"></path><path class="cls-1" d="M48,66.58v5.68a3.4,3.4,0,0,1-3.34,3.46h0a3.4,3.4,0,0,1-3.34-3.45h0V65.58" transform="translate(-1.93 -15.75)"></path><polyline class="cls-1" points="28.75 48.05 22.66 59.3 13.83 65.61 14.41 54.5 19.11 45.17"></polyline><polyline class="cls-1" points="25.17 34.59 43.75 0.25 52.01 5.04 36.39 33.91"></polyline><line class="cls-1" x1="0.25" y1="66.92" x2="13.83" y2="66.92"></line></g></g></svg>`;
+				// const generateSVG= `<svg viewBox="0 0 100 100" class="svg-icon GENERATE_ICON"><defs><style>.cls-1{fill:none;stroke:currentColor;stroke-linecap:round;stroke-linejoin:round;stroke-width:4px;}</style></defs><g id="Layer_2" data-name="Layer 2"><g id="VECTOR"><rect class="cls-1" x="74.98" y="21.55" width="18.9" height="37.59"></rect><path class="cls-1" d="M38.44,27.66a8,8,0,0,0-8.26,1.89L24.8,34.86a25.44,25.44,0,0,0-6,9.3L14.14,56.83C11.33,64.7,18.53,67.3,21,60.9" transform="translate(-1.93 -15.75)"></path><polyline class="cls-1" points="74.98 25.58 56.61 18.72 46.72 15.45"></polyline><path class="cls-1" d="M55.45,46.06,42.11,49.43,22.76,50.61c-8.27,1.3-5.51,11.67,4.88,12.8L46.5,65.78,53,68.4a23.65,23.65,0,0,0,17.9,0l6-2.46" transform="translate(-1.93 -15.75)"></path><path class="cls-1" d="M37.07,64.58v5.91A3.49,3.49,0,0,1,33.65,74h0a3.49,3.49,0,0,1-3.45-3.52V64.58" transform="translate(-1.93 -15.75)"></path><path class="cls-1" d="M48,66.58v5.68a3.4,3.4,0,0,1-3.34,3.46h0a3.4,3.4,0,0,1-3.34-3.45h0V65.58" transform="translate(-1.93 -15.75)"></path><polyline class="cls-1" points="28.75 48.05 22.66 59.3 13.83 65.61 14.41 54.5 19.11 45.17"></polyline><polyline class="cls-1" points="25.17 34.59 43.75 0.25 52.01 5.04 36.39 33.91"></polyline><line class="cls-1" x1="0.25" y1="66.92" x2="13.83" y2="66.92"></line></g></g></svg>`;
+				const generateSVG = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="2" x2="22" y2="6"></line><path d="M7.5 20.5 19 9l-4-4L3.5 16.5 2 22z"></path></svg>`
 
 				const button = this.createRunButton("Generate Text",generateSVG);
 				button.addEventListener("click", async () => {
